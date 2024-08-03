2dd47507e82c68c5b12c7289476938f80150009b
Mitesh Birru <79537705+mitibirru@users.noreply.github.com>
2024-08-04 01:20:56 +0530

    image carousels and detail page done

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,54 @@
+import './filter.scss';
+
+function Filter() {
+	return (
+		<div className="filter">
+			<h1>
+				Search results for <b>London</b>
+			</h1>
+			<div className="top">
+				<div className="item">
+					<label htmlFor="city">Location</label>
+					<input type="text" id="city" name="city" placeholder="City Placeholder" />
+				</div>
+			</div>
+			<div className="bottom">
+				<div className="item">
+					<label htmlFor="type">Type</label>
+					<select name="type" id="type">
+						<option value="any">any</option>
+						<option value="buy">Buy</option>
+						<option value="rent">Rent</option>
+					</select>
+				</div>
+				<div className="item">
+					<label htmlFor="property">Property</label>
+					<select name="type" id="type">
+						<option value="any">any</option>
+						<option value="apartment">Apartment</option>
+						<option value="house">House</option>
+						<option value="condo">Condo</option>
+						<option value="land">Land</option>
+					</select>
+				</div>
+				<div className="item">
+					<label htmlFor="minPrice">Min Price</label>
+					<input type="number" id="minPrice" name="minPrice" placeholder="any" />
+				</div>
+				<div className="item">
+					<label htmlFor="maxPrice">Max Price</label>
+					<input type="number" id="maxPrice" name="maxPrice" placeholder="any" />
+				</div>
+				<div className="item">
+					<label htmlFor="bedroom">Bedroom</label>
+					<input type="number" id="bedroom" name="bedroom" placeholder="any" />
+				</div>
+				<button>
+					<img src="/search.png" />
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Filter;
--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,7 +1,26 @@
+import Card from '../../components/card/card';
+import Filter from '../../components/filter/filter';
+import Map from '../../components/map/map';
+import { listData } from '../../lib/dummyData';
 import './listPage.scss';
 
 function ListPage() {
-	return <div></div>;
+	const data = listData;
+	return (
+		<div className="listPage">
+			<div className="listContainer">
+				<div className="wrapper">
+					<Filter />
+					{data.map(item => (
+						<Card key={item.id} item={item} />
+					))}
+				</div>
+			</div>
+			<div className="mapContainer">
+				<Map items={data} />
+			</div>
+		</div>
+	);
 }
 
 export default ListPage;
